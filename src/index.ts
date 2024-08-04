738d7cbe95032b29de9129c85245d1bc967dbd49
platinumazure <platinumazure@gmail.com>
2024-08-04 09:29:04 -0500

    Add endpoints to create and retrieve Discord server info

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,39 @@
 import express from 'express';
-import { GoogleSheetsProvider } from './lib/providers/googleSheets/googleSheetsProvider.js';
+import bodyParser from 'body-parser';
 
-// Has side effects (TODO: Move this logic to an app initialization script later)
+import { GoogleSheetsProvider } from './lib/providers/googleSheets/googleSheetsProvider.js';
 import { SQLiteProvider } from './lib/database/sqlite3.js';
 
 const app = express();
+app.use(bodyParser.json());
+
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+});
+
+// Get Discord server information by Discord server ID
+app.get('/servers/:discordServerId', async (req, res) => {
+  const discordServerId = req.params.discordServerId;
+
+  const sqliteProvider = new SQLiteProvider();
+  const server = await sqliteProvider.getDiscordServer(discordServerId);
+
+  res.send(server);
+});
+
+app.post('/servers', async (req, res) => {
+  const discordServerId = req.body.discordServerId;
+  const discordServerName = req.body.discordServerName;
+
+  const sqliteProvider = new SQLiteProvider();
+  const createdServer = await sqliteProvider.insertDiscordServer(
+    discordServerId,
+    discordServerName,
+  );
+
+  res.send(createdServer);
 });
 
 app.get('/check/google-sheets/:discordHandle', async (req, res) => {
